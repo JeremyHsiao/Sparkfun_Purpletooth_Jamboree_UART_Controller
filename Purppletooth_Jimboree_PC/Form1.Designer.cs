ccf6851b2bb0eb68deffd340ff974df08a14a29f
Jeremy Hsiao <jeremy.hsiao@outlook.com>
2015-08-03 16:43:18 +0800

    Add basic Serial R/W code as base for further development
    UART serial can be read/write now.

---
+++ Purppletooth_Jimboree_PC/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Purppletooth_Jimboree_PC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnFreshCOMNo = new System.Windows.Forms.Button();
+            this.btnConnectionControl = new System.Windows.Forms.Button();
+            this.txtStringToSerial = new System.Windows.Forms.TextBox();
+            this.btnSendStringToUART = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(76, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(69, 40);
+            this.listBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(470, 327);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnFreshCOMNo
+            // 
+            this.btnFreshCOMNo.Location = new System.Drawing.Point(12, 12);
+            this.btnFreshCOMNo.Name = "btnFreshCOMNo";
+            this.btnFreshCOMNo.Size = new System.Drawing.Size(58, 40);
+            this.btnFreshCOMNo.TabIndex = 2;
+            this.btnFreshCOMNo.Text = "Refresh COM";
+            this.btnFreshCOMNo.UseVisualStyleBackColor = true;
+            this.btnFreshCOMNo.Click += new System.EventHandler(this.RefreshCOMPortNumber_Click);
+            // 
+            // btnConnectionControl
+            // 
+            this.btnConnectionControl.Location = new System.Drawing.Point(151, 12);
+            this.btnConnectionControl.Name = "btnConnectionControl";
+            this.btnConnectionControl.Size = new System.Drawing.Size(67, 40);
+            this.btnConnectionControl.TabIndex = 3;
+            this.btnConnectionControl.Text = "Connect";
+            this.btnConnectionControl.UseVisualStyleBackColor = true;
+            this.btnConnectionControl.Click += new System.EventHandler(this.ConnectionButton_Click);
+            // 
+            // txtStringToSerial
+            // 
+            this.txtStringToSerial.BackColor = System.Drawing.SystemColors.Window;
+            this.txtStringToSerial.Location = new System.Drawing.Point(12, 403);
+            this.txtStringToSerial.Name = "txtStringToSerial";
+            this.txtStringToSerial.Size = new System.Drawing.Size(399, 22);
+            this.txtStringToSerial.TabIndex = 4;
+            this.txtStringToSerial.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtStringToSerial_KeyDown);
+            // 
+            // btnSendStringToUART
+            // 
+            this.btnSendStringToUART.Location = new System.Drawing.Point(417, 403);
+            this.btnSendStringToUART.Name = "btnSendStringToUART";
+            this.btnSendStringToUART.Size = new System.Drawing.Size(65, 22);
+            this.btnSendStringToUART.TabIndex = 5;
+            this.btnSendStringToUART.Text = "Send";
+            this.btnSendStringToUART.UseVisualStyleBackColor = true;
+            this.btnSendStringToUART.Click += new System.EventHandler(this.btnSendStringToUART_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 433);
+            this.Controls.Add(this.btnSendStringToUART);
+            this.Controls.Add(this.txtStringToSerial);
+            this.Controls.Add(this.btnConnectionControl);
+            this.Controls.Add(this.btnFreshCOMNo);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "PurpleTooth Jamboree";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnFreshCOMNo;
+        private System.Windows.Forms.Button btnConnectionControl;
+        private System.Windows.Forms.TextBox txtStringToSerial;
+        private System.Windows.Forms.Button btnSendStringToUART;
+    }
+}
+
---
+++ Purppletooth_Jimboree_PC/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Purppletooth_Jimboree_PC
+{
+     public partial class Form1 : Form
+    {
+        static SerialPort _serialPort;
+        
+         public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _serialPort = new SerialPort();
+            Serial_InitialSetting();
+            Serial_UpdatePortName();
+            DisableSendStringButton();
+        }
+
+        private void Serial_InitialSetting()
+        {
+            // Allow the user to set the appropriate properties.
+            _serialPort.PortName = "COM42";
+            _serialPort.BaudRate = 9600;
+            _serialPort.Parity = Parity.None;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Handshake = Handshake.None;
+            _serialPort.Encoding = Encoding.UTF8;
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+        }
+
+        private void Serial_UpdatePortName()
+        {
+            listBox1.Items.Clear();
+            foreach (string comport_s in SerialPort.GetPortNames())
+            {
+                listBox1.Items.Add(comport_s);
+            }
+            if (listBox1.Items.Count > 0)
+            {
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                EnableConnectButton();
+                UpdateToConnectButton();
+            }
+            else
+            {
+                DisableConnectButton();
+                UpdateToConnectButton();
+            }
+        }
+
+        private Boolean Serial_OpenPort(string PortName)
+        {
+            Boolean ret = false;
+            try
+            {
+                _serialPort.PortName = PortName;
+                _serialPort.Open();
+                ret = true;
+            }
+            catch (Exception ex232)
+            {
+                ret = false;
+            }
+            return ret;
+        }
+
+        private Boolean Serial_ClosePort()
+        {
+            Boolean ret = false;
+            try
+            {
+                _serialPort.Close();
+                ret = true;
+            }
+            catch (Exception ex232)
+            {
+                ret = false;
+            }
+            return ret;
+        }
+
+        private void Serial_WriteStringWithNewLine(string out_str)
+        {
+            if (_serialPort.IsOpen == true)
+            {
+                try
+                {
+                    _serialPort.WriteLine(out_str);
+                }
+                catch (TimeoutException timeout)
+                {
+                    MessageBox.Show(timeout.ToString());
+                }
+            }
+            else
+            {
+                AppendSerialMessageLog("COM port is not connected!\n");
+            }
+        }
+
+        private void Serial_WriteBufferWithIndexLength(byte[] data_buffer, int offset, int count)
+        {
+            if (_serialPort.IsOpen == true)
+            {
+                try
+                {
+                    _serialPort.Write(data_buffer, offset, count);
+                }
+                catch (TimeoutException timeout)
+                {
+                    MessageBox.Show(timeout.ToString());
+                }
+            }
+        }
+
+         //
+         // Print Serial Port Message on RichTextBox
+         //
+        delegate void AppendSerialMessageCallback(string text);
+        public void AppendSerialMessageLog(string my_str)
+        {
+            if (this.richTextBox1.InvokeRequired)
+            {
+                AppendSerialMessageCallback d = new AppendSerialMessageCallback(AppendSerialMessageLog);
+                this.Invoke(d, new object[] { my_str });
+            }
+            else
+            {
+                this.richTextBox1.AppendText(my_str);
+                this.richTextBox1.ScrollToCaret();
+            }
+        }
+
+        static bool _continue_serial_read_write=false;
+        static Thread readThread = null;
+
+        public void ReadSerialPortThread()
+        {
+            while (_continue_serial_read_write)
+            {
+                try
+                {
+                    int DataLength = _serialPort.BytesToRead;
+                    if (DataLength > 0)
+                    {
+                        string message = _serialPort.ReadExisting();
+                        this.AppendSerialMessageLog(message);
+                    }
+                }
+                catch (TimeoutException) { }
+            }
+        }
+        private void Start_SerialReadThread()
+        {
+            _continue_serial_read_write = true;
+            readThread = new Thread(ReadSerialPortThread);
+            readThread.Start();
+        }
+        private void Stop_SerialReadThread()
+        {
+            _continue_serial_read_write = false;
+            if (readThread != null)
+            {
+                if (readThread.IsAlive)
+                {
+                    readThread.Join();
+                }
+            }
+        }
+
+        private void EnableSendStringButton()
+        {
+            btnSendStringToUART.Enabled = true;
+            txtStringToSerial.BackColor = Color.White;
+        }
+
+        private void DisableSendStringButton()
+        {
+            btnSendStringToUART.Enabled = false;
+            txtStringToSerial.BackColor = Color.Gray;
+        }
+
+        private void EnableRefreshCOMButton()
+        {
+            btnFreshCOMNo.Enabled = true;
+        }
+
+        private void DisableRefreshCOMButton()
+        {
+            btnFreshCOMNo.Enabled = false;
+        }
+
+        private void EnableConnectButton()
+        {
+            btnConnectionControl.Enabled = true;
+        }
+
+        private void DisableConnectButton()
+        {
+            btnConnectionControl.Enabled = false;
+        }
+
+        private void UpdateToConnectButton()
+        {
+            btnConnectionControl.Text = "Connect";
+            btnIsConnectionButton = true;
+        }
+
+        private void UpdateToDisconnectButton()
+        {
+            btnConnectionControl.Text = "Disconnect";
+            btnIsConnectionButton = false;
+        }
+
+        private void RefreshCOMPortNumber_Click(object sender, System.EventArgs e)
+        {
+            Serial_UpdatePortName();
+        }
+
+        Boolean btnIsConnectionButton = true;
+        private void ConnectionButton_Click(object sender, System.EventArgs e)
+        {
+            if (btnIsConnectionButton==true)
+            {   // User to connect
+                if (_serialPort.IsOpen == false)
+                {
+                    string curItem = listBox1.SelectedItem.ToString();
+                    if (Serial_OpenPort(curItem) == true)
+                    {
+                        UpdateToDisconnectButton();
+                        DisableRefreshCOMButton();
+                        EnableSendStringButton();
+                        Start_SerialReadThread();
+                    }
+                    else
+                    {
+                        richTextBox1.AppendText(DateTime.Now.ToString("h:mm:ss tt") + " - Cannot connect to RS232.\n");
+                    }
+                }
+            }
+            else
+            {   // User to disconnect
+                if (_serialPort.IsOpen == true)
+                {
+                    Stop_SerialReadThread();
+                    if (Serial_ClosePort() == true)
+                    {
+                        UpdateToConnectButton();
+                        EnableRefreshCOMButton();
+                        DisableSendStringButton();
+                    }
+                    else
+                    {
+                        richTextBox1.AppendText(DateTime.Now.ToString("h:mm:ss tt") + " - Cannot disconnect from RS232.\n");
+                    }
+                }
+            }
+        }
+
+        private void btnSendStringToUART_Click(object sender, System.EventArgs e)
+        {
+            string temp_str = txtStringToSerial.Text;
+
+            if(temp_str != "")
+            {
+                Serial_WriteStringWithNewLine(temp_str);
+            }
+        }
+
+        private void txtStringToSerial_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string temp_str = txtStringToSerial.Text;
+
+                if (temp_str != "")
+                {
+                    Serial_WriteStringWithNewLine(temp_str);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_serialPort.IsOpen == true)
+            {
+                Stop_SerialReadThread();
+            }
+            UpdateToConnectButton();
+        }
+    }
+
+ 
+}
