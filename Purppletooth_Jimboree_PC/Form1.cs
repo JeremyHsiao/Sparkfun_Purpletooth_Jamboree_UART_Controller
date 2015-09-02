fe04ecf41e90f94dd47d94c1ffe7f8d19f283d10
Jeremy Hsiao <jeremy.hsiao@outlook.com>
2015-09-02 13:35:01 +0800

    Implement parser for returning information by "config" command

--- Purppletooth_Jimboree_PC/Form1.cs
+++ Purppletooth_Jimboree_PC/Form1.cs
@@ -406,8 +406,145 @@ namespace Purppletooth_Jimboree_PC
             Serial_UpdateBaudRate();
         }
 
-        private void btnGetConfig_click(object sender, EventArgs e)
+        class PTJ_Configuration
         {
+            public string audio { get; set; }
+            public string autoconn { get; set; }
+            public string batt_thresh { get; set; }
+            public string baud { get; set; }
+            public string ble_role { get; set; }
+            public string bps { get; set; }
+            public string classic_role { get; set; }
+            public string cmd_to { get; set; }
+            public string cod { get; set; }
+            public string codec { get; set; }
+            public string deep_sleep { get; set; }
+            public string device_id { get; set; }
+            public string discoverable { get; set; }
+            public string timeout { get; set; }
+            public string enable_a2dp { get; set; }
+            public string enable_android_ble { get; set; }
+            public string enable_avrcp { get; set; }
+            public string enable_batt_ind { get; set; }
+            public string enable_hfp { get; set; }
+            public string enable_hfp_cvc { get; set; }
+            public string enable_hfp_nrec { get; set; }
+            public string enable_hfp_wbs { get; set; }
+            public string enable_led { get; set; }
+            public string enable_map { get; set; }
+            public string enable_pbap { get; set; }
+            public string enable_spp { get; set; }
+            public string enable_spp_sniff { get; set; }
+            public string flow_ctrl { get; set; }
+            public string force_analog_mic { get; set; }
+            public string gpiocontrol { get; set; }
+            public string i2s { get; set; }
+            public string input_gain { get; set; }
+            public string local_addr { get; set; }
+            public string max_rec { get; set; }
+            public string music_meta_data { get; set; }
+            public string name { get; set; }
+            public string name_short { get; set; }
+            public string parity { get; set; }
+            public string pin { get; set; }
+            public string remote_addr { get; set; }
+            public string rssi_thresh { get; set; }
+            public string spp_transparent { get; set; }
+            public string uuid_data { get; set; }
+            public string uuid_spp { get; set; }
+            public string uuid_srv { get; set; }
+
+            /*
+                        public int audio { get; set; }
+                        public int autoconn{ get; set; }
+                        public int batt_thresh_charglvl{ get; set; }
+                        public int batt_thresh_crit{ get; set; }
+                        public int batt_thresh_low{ get; set; }
+                        public int batt_thresh_lvl0{ get; set; }
+                        public int batt_thresh_lvl1{ get; set; }
+                        public int batt_thresh_lvl2{ get; set; }
+                        public int batt_thresh_lvl3{ get; set; }
+                        public int baud{ get; set; }
+                        public int ble_role{ get; set; }
+                        public int bps{ get; set; }
+                        public int classic_role{ get; set; }
+                        public int cmd_t0{ get; set; }
+                        public int cod{ get; set; }
+                        public int codec_codec{ get; set; }
+                        public int codec_fs{ get; set; }
+                        public int codec_mode{ get; set; }
+                        public int seep_sleep{ get; set; }
+                        public int device_id_vendor_id_source{ get; set; }
+                        public int device_id_vendor_id{ get; set; }
+                        public int device_id_product_id{ get; set; }
+                        public int device_id_bcd_version{ get; set; }
+                        public ulong device_id_software_version{ get; set; }
+                        public int discoverable_value{ get; set; }
+                        public int timeout{ get; set; }
+                        public bool enable_a2dp{ get; set; }
+                        public bool enable_android_ble{ get; set; }
+                        public bool enable_avrcp{ get; set; }
+                        public bool enable_battery_ind{ get; set; }
+                        public bool enable_hfp{ get; set; }
+                        public bool enable_hfp_cvc{ get; set; }
+                        public bool enable_hfp_nrec{ get; set; }
+                        public bool enable_hfp_wbs{ get; set; }
+                        public bool enable_led{ get; set; }
+                        public bool enable_map{ get; set; }
+                        public bool enable_pbap{ get; set; }
+                        public bool enable_spp{ get; set; }
+                        public bool enable_spp_sniff_state{ get; set; }
+                        public int enable_spp_sniff_min_interval{ get; set; }
+                        public int enable_spp_sniff_max_interval{ get; set; }
+                        public int enable_spp_sniff_attempt{ get; set; }
+                        public int enable_spp_sniff_timeout{ get; set; }
+                        public int enable_spp_sniff_duration{ get; set; }
+                        public bool flow_ctrl{ get; set; }
+                        public int force_analog_mic{ get; set; }
+                        public bool gpio_control{ get; set; }
+                        public bool i2s{ get; set; }
+                        public int intput_gain{ get; set; }
+                        public int local_addr{ get; set; }
+                        public int max_rec{ get; set; }
+                        public bool music_meta_data{ get; set; }
+                        public string name{ get; set; }
+                        public string name_short{ get; set; }
+                        public int parity{ get; set; }
+                        public string pin{ get; set; }
+                        public long remote_addr{ get; set; }
+            */
+        }
+
+        private void ParseConfigQueue()
+        {
+            PTJ_Configuration board_config = new PTJ_Configuration();
+            char[] GetConfigCharSeparators = new char[] { '=', '\r' };
+            foreach (string str in UART_MSG)
+            {
+                string[] words;
+                words = str.Split(GetConfigCharSeparators, StringSplitOptions.RemoveEmptyEntries);
+                if (words.Length >= 2)
+                {
+                    // Combine other splitting word,if any, into one parameter
+                    for(int index=2; index<words.Length; index++)
+                    {
+                        words[1] += words[index];
+                    }
+                    AppendSerialMessageLog(words[0] + "/" + words[1] + '\x0d');
+                    // Use "string content" as "property name" --> easy for extension and coding
+                    System.Reflection.PropertyInfo prop = typeof(PTJ_Configuration).GetProperty(words[0].ToLower());
+                    prop.SetValue(board_config, words[1], null);
+                    // the following code is only for reference
+                    //object value = prop.GetValue(yourInstance, null);
+                    //prop.SetValue(yourInstance, "value", null);
+                }
+            }
+            //AppendSerialMessageLog("FIN.\r");
+        }
+
+
+        private void btnGetConfig_click(object sender, EventArgs e)
+        { 
             Enable_ReadLine_Queue();
             Clear_ReadLine_Timeout_Flag();
             Serial_WriteStringWithPause("config\x0d");
@@ -417,16 +554,7 @@ namespace Purppletooth_Jimboree_PC
             }      
             Disable_ReadLine_Queue();
 
-            char[] charSeparators = new char[] { '=', '\r' };
-            foreach (string str in UART_MSG)
-            {
-                string[] words;
-                words = str.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
-                if (words.Length >= 2)
-                {
-                    AppendSerialMessageLog( words[0] + "/" + words[1] + '\x0d');
-                }
-            }
+            ParseConfigQueue();
         }
 
     }
